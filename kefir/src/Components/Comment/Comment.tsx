6cc2304c4cf001b7964899cd6ecf6c196a027f3b
Berd <berd@digitalberd.com>
2023-10-04 17:22:52 +0300

    продолжила заниматься версткой комментария

--- kefir/src/Components/Comment/Comment.tsx
+++ kefir/src/Components/Comment/Comment.tsx
@@ -12,11 +12,24 @@ const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 function Comment () {
     return (
         <div className={comment.comment_container}>
-          <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
-          <h5>User Name</h5>
-          <p>вчера в 20:07</p>
-          <p>Эксперимент в подготовки позволяет количественный условий особенности показывает, способствует соответствующий проверки проверки финансовых обучения позволяет и играет активности нашей практика развит</p>
-          <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          <div className={comment.avatar}>
+            <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+          </div>
+
+          <div className={comment.head}>
+            <div className={comment.info}>
+             <h5>User Name</h5>
+             <p>вчера в 20:07</p>
+            </div>
+
+            <div className={comment.like}>
+            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+            </div>
+          </div>
+
+            <div className={comment.comment}>
+            <p>Эксперимент в подготовки позволяет количественный условий особенности показывает, способствует соответствующий проверки проверки финансовых обучения позволяет и играет активности нашей практика развит</p>
+            </div>
         </div>
     )
 }
